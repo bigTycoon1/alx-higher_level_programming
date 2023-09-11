c23b332521f508aa4c130a3562e4f282e167dac3
bigTycoon1 <mztatycoon@gmail.com>
2023-09-11 03:24:33 -0700

    pythonData

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def no_c(my_string):
-    result = ""
+    new_string = ""
+
     for char in my_string:
-        if char != 'c' and char != 'C':
-            result += char
-    return (result)
+        if char.lower() != 'c':
+            new_string += char
+
+    return (new_string)
