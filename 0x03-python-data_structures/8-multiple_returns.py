dec7ce1fd8f1e78751cf5b3ca7a048a1b4d7a64b
bigTycoon1 <mztatycoon@gmail.com>
2023-09-11 04:07:04 -0700

    pythonData

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    len_sen = len(sentence)
+
+    if len_sen == 0:
+        return (len_sen, None)
+    else:
+        return (len_sen, sentence[0])
