b8ef8c067224bfe8eb11f6e35af0cdc0d6cfbfaf
bigTycoon1 <mztatycoon@gmail.com>
2023-10-10 15:48:25 -0700

    imput/output

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""Modules that defines a JSON file-writing function."""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+
+    """Write an object to a text file. """
+
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
