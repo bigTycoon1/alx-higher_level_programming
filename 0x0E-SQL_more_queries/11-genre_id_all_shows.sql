fe270b870c804429e5f2ad2afc2871fd79b2649d
bigTycoon1 <mztatycoon@gmail.com>
2024-01-18 23:58:52 -0800

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- Script that lists all cities contained in the database hbtn_0d_tvshows.
+SELECT tv_shows.title, tv_show_genres.genre_id -- Query to combine cities and states
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
