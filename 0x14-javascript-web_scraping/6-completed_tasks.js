720dcbdada00c787542cc5f752e66b5157e84c19
bigTycoon1 <mztatycoon@gmail.com>
2024-03-27 05:23:54 -0700

    0x14. JavaScript - Web scraping

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    const res = {};
+    const json = JSON.parse(body);
+    for (let i = 0; i < json.length; i++) {
+      if (json[i].completed === true) {
+        if (res[json[i].userId] === undefined) {
+          res[json[i].userId] = 0;
+        }
+        res[json[i].userId]++;
+      }
+    }
+    console.log(res);
+  }
+});
